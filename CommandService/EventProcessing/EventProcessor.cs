11023047eebb9780c6af3bcd8795e7c03035b6d9
Minko Minkov <minccccc.mm@gmail.com>
2022-02-01 23:09:14 +0200

    Fix a comment

--- CommandService/EventProcessing/EventProcessor.cs
+++ CommandService/EventProcessing/EventProcessor.cs
@@ -63,6 +63,8 @@ namespace CommandService.EventProcessing
                     {
                         repo.CreatePlatform(plat);
                         repo.SaveChanges();
+
+                        Console.WriteLine($"--> Platform added!");
                     }
                     else
                     {
